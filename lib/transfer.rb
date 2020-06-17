32d8885b9dd21d2c63bbc244d1f7639477f30094
Sajid Anjum <sajid1760@gmail.com>
2020-06-17 01:06:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -74,7 +74,7 @@ end
 amanda = BankAccount.new(amanda)
 terrance = BankAccount.new(terrance)
 #terrance.close_account
-puts amanda.status
-puts terrance.status
+amanda.status
+terrance.status
 closed_account_transfer = Transfer.new(amanda, terrance, 50)
 closed_account_transfer.execute_transaction
