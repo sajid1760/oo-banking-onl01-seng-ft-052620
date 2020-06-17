910b277cae219c6c9fa3df076cf6ed8a7dea765c
Sajid Anjum <sajid1760@gmail.com>
2020-06-17 00:53:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ attr_accessor :sender, :receiver, :status, :amount
 
  def execute_transaction
    if @status == "pending" then
-     if sender.balance >= amount then
+     if sender.balance > amount then
        sender.deposit(0 - @amount)
        receiver.deposit(@amount)
        @status = "complete"
